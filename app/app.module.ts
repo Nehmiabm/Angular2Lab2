de348b2c5830dddfd0602ec289dc6cfbb96029c6
nehmiabm <983151@CS.MUM.EDU>
2016-10-21 15:20:14 -0500

    inital commit

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { ProductDetailComponent } from './product-detail.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent,ProductDetailComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/product.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { PRODUCTS } from './mock-products';
+import { Product } from './product';
+@Injectable()
+export class ProductService {
+    getProducts(): Product[] { return PRODUCTS } // stub
+}
